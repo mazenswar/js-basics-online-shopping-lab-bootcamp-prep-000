47f40b3834d32252f0ccd42be5da5d08b53935e1
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-10 18:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,6 +64,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    return ""
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  else {
+    cart =[];
+    return ``
   }
 }
